3c2f12f58b71f31491eed9797c2bc1b9d1fbc683
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 08:01:02 +0200

    Refactor GUI.py for improved readability and maintainability

--- GUI.py
+++ GUI.py
@@ -1,33 +1,40 @@
 from card import Card
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode('system')
-# customtkinter.set_theme('light')
+# Set appearance mode
+ctk.set_appearance_mode('system')
+def redeem_card():
+    check_card_error_message.configure(text='')
+    card_number = card_number_input.get()
+    already_redeemed = Card.check_redemption(card_number)
+    if already_redeemed:
+        check_card_error_message.configure(text='Card has already been redeemed')
+        return
+    card_value = Card.redeem_card(card_number)
+    check_card()
 
-root = customtkinter.CTk()
-root.title('Card Machine')
+def check_card():
+    check_card_error_message.configure(text='')
+    card_number = card_number_input.get()
+    try:
+        card_serial, card_value = Card.get_card_info(card_number)
+    except ValueError as e:
+        check_card_error_message.configure(text=str(e))
+        return
 
-main_frame = customtkinter.CTkFrame(root)
-main_frame.pack(fill='both', expand=True)
+    redeemed = Card.check_redemption(card_number)
+    redeemed_text = 'Redeemed' if redeemed else 'Not Redeemed'
 
-# create buy card section
-buy_card_frame = customtkinter.CTkFrame(main_frame)
-buy_card_frame.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
+    card_value_text.configure(state='normal')
+    card_status_text.configure(state='normal')
+    card_value_text.delete('1.0', 'end')
+    card_value_text.insert('1.0', card_value)
+    card_status_text.delete('1.0', 'end')
+    card_status_text.insert('1.0', redeemed_text)
+    card_value_text.configure(state='disabled')
+    card_status_text.configure(state='disabled')
 
-buy_card_label = customtkinter.CTkLabel(buy_card_frame, text='Buy Card', font=('Arial', 20))
-buy_card_label.grid(row=0, column=0, pady=10, padx=10)
-
-buy_card_error_message = customtkinter.CTkLabel(buy_card_frame, text='', text_color='red')
-buy_card_error_message.grid(row=1, column=0, pady=10)
-
-card_value_input = customtkinter.CTkEntry(buy_card_frame, placeholder_text='Card Value', width=200)
-card_value_input.grid(row=2, column=0, pady=10)
-
-card_number_label = customtkinter.CTkLabel(buy_card_frame, text='Card Number: ')
-card_number_label.grid(row=3, column=0, pady=10)
-
-card_number_value = customtkinter.CTkTextbox(buy_card_frame, height=1, state='disabled')
-card_number_value.grid(row=4, column=0, pady=10)
+    card_value_label['text'] = 'Card Value: ' + str(card_value)
 
 def buy_card():
     buy_card_error_message.configure(text='')
@@ -39,80 +46,70 @@ def buy_card():
         return
 
     card = Card.generate_card_number(card_value)
-
     card_number_value.configure(state='normal')
     card_number_value.delete('1.0', 'end')
     card_number_value.insert('1.0', card)
     card_number_value.configure(state='disabled')
-
     card_value_input.delete(0, 'end')
 
-buy_card_button = customtkinter.CTkButton(buy_card_frame, text='Buy Card', command=buy_card)
-buy_card_button.grid(row=5, column=0, pady=10, padx=10)
+def create_label(frame, text, font = 'Arial', font_size = 20, row=0, column=0, pady=10, padx=10, text_color=None, wraplength=0):
+    label = ctk.CTkLabel(frame, text=text, font=(font, font_size), wraplength=wraplength)
+    label.grid(row=row, column=column, pady=pady, padx=padx)
+    if text_color:
+        label.configure(text_color=text_color)
+    return label
 
-# check and redeem card section
-check_card_frame = customtkinter.CTkFrame(main_frame)
-check_card_frame.grid(row=0, column=1, sticky='nsew', padx=10, pady=10)
 
-check_card_label = customtkinter.CTkLabel(check_card_frame, text='Card Info', font=('Arial', 20))
-check_card_label.grid(row=0, column=0, pady=10, padx=10)
+def create_entry(frame, placeholder_text, width=140, height=28, row=0, column=0, pady=10, padx=10, font_size=12, font='Arial'):
+    entry = ctk.CTkEntry(frame, placeholder_text=placeholder_text, width=width, height=height, font=(font, font_size))
+    entry.grid(row=row, column=column, pady=pady, padx=padx)
+    return entry
 
-check_card_error_message = customtkinter.CTkLabel(check_card_frame, text='', text_color='red')
-check_card_error_message.grid(row=1, column=0, pady=10)
+def create_textbox(frame, height, width, state, row=0, column=0, pady=10, font_size=12, font='Arial'):
+    textbox = ctk.CTkTextbox(frame, height=height, width=width, state=state, font=(font, font_size))
+    textbox.grid(row=row, column=column, pady=pady)
+    return textbox
 
-card_number_input = customtkinter.CTkEntry(check_card_frame, placeholder_text='Card Number', width=200)
-card_number_input.grid(row=2, column=0, pady=10)
+def create_button(frame, text, command, row=0, column=0, pady=10, padx=10, width=140, height=28, font_size=12, font='Arial'):
+    button = ctk.CTkButton(frame, text=text, command=command, width=width, height=height, font=(font, font_size))
+    button.grid(row=row, column=column, pady=pady, padx=padx)
+    return button
 
-card_value_label = customtkinter.CTkLabel(check_card_frame, text='Card Value: ')
-card_value_label.grid(row=3, column=0, pady=10)
+# Create the main window
+root = ctk.CTk()
+root.title('Recharge Cards System')
 
-card_value_text = customtkinter.CTkTextbox(check_card_frame, height=1, width=50, state='disabled')
-card_value_text.grid(row=4, column=0, pady=10)
+# Create main frame
+main_frame = ctk.CTkFrame(root)
+main_frame.pack(fill='both', expand=True)
 
-card_status_label = customtkinter.CTkLabel(check_card_frame, text='Card Status: ')
-card_status_label.grid(row=5, column=0, pady=10)
+title_label = create_label(main_frame, 'Recharge Cards System', font_size=40, pady=10)
+title_label.grid(row=0, column=0, columnspan=2)
 
-card_status_text = customtkinter.CTkTextbox(check_card_frame, height=1, width=100, state='disabled')
-card_status_text.grid(row=6, column=0, pady=10)
+# Create buy card section
+buy_card_frame = ctk.CTkFrame(main_frame)
+buy_card_frame.grid(row=2, column=0, sticky='nsew', padx=30, pady=20)
 
-def check_card():
-    check_card_error_message.configure(text='')
-    card_number = card_number_input.get()
-    try:
-        card_serial, card_value = Card.get_card_info(card_number)
-    except ValueError as e:
-        check_card_error_message.configure(text=str(e))
-        return
-    redeemed = Card.check_redemption(card_number)
-    redeemed_text = 'Redeemed' if redeemed else 'Not Redeemed'
+buy_card_label = create_label(buy_card_frame, 'Buy Card', font_size=30, pady=20)
+buy_card_error_message = create_label(buy_card_frame, '', font_size=12, text_color='red', row=1, wraplength=250)
+card_value_input = create_entry(buy_card_frame, 'Card Value', 240, row=2, padx=30,height=35, font_size=16)
+buy_card_button = create_button(buy_card_frame, 'Buy Card', buy_card, row=3, width=240, height=40, pady=20, font_size=16)
+card_number_label = create_label(buy_card_frame, 'Card Number', row=4, font_size=16, pady=0)
+card_number_value = create_textbox(buy_card_frame, 1, 240, 'disabled', row=5)
 
-    card_value_text.configure(state='normal')
-    card_value_text.delete('1.0', 'end')
-    card_value_text.insert('1.0', card_value)
-    card_value_text.configure(state='disabled')
+# Create check and redeem card section
+check_card_frame = ctk.CTkFrame(main_frame)
+check_card_frame.grid(row=2, column=1, sticky='nsew', padx=30, pady=20)
 
-    card_status_text.configure(state='normal')
-    card_status_text.delete('1.0', 'end')
-    card_status_text.insert('1.0', redeemed_text)
-    card_status_text.configure(state='disabled')
+check_card_label = create_label(check_card_frame, 'Card Info', font_size=30, pady=20)
+check_card_error_message = create_label(check_card_frame, '', font_size=12, text_color='red', row=1, wraplength=250)
+card_number_input = create_entry(check_card_frame, 'Card Number', 240, row=2, padx=30, height=35, font_size=16)
+check_card_button = create_button(check_card_frame, 'Check Card', check_card, row=3, width=240, height=40, pady=20, font_size=16)
+card_value_label = create_label(check_card_frame, 'Card Value', row=4,font_size=16, pady=0)
+card_value_text = create_textbox(check_card_frame, 1, 260, 'disabled', row=5)
+card_status_label = create_label(check_card_frame, 'Card Status:', row=6, font_size=16, pady=0)
+card_status_text = create_textbox(check_card_frame, 1, 260, 'disabled', row=7)
+redeem_card_button = create_button(check_card_frame, 'Redeem Card', redeem_card, row=8, width=240, padx=30, height=35, font_size=16)
 
-    card_value_label['text'] = 'Card Value: ' + str(card_value)
-
-check_card_button = customtkinter.CTkButton(check_card_frame, text='Check Card', command=check_card)
-check_card_button.grid(row=7, column=0, pady=10, padx=10)
-
-def redeem_card():
-    check_card_error_message.configure(text='')
-
-    already_redeemed = Card.check_redemption(card_number_input.get())
-    if already_redeemed:
-        check_card_error_message.configure(text='Card has already been redeemed')
-        return
-    card_number = card_number_input.get()
-    card_value = Card.redeem_card(card_number)
-    check_card()
-
-redeem_card_button = customtkinter.CTkButton(check_card_frame, text='Redeem Card', command=redeem_card)
-redeem_card_button.grid(row=8, column=0, pady=10, padx=10)
 
 root.mainloop()
