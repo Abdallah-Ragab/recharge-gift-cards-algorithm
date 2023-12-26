c450cafc62819bf625d0832ab0e77d77f9040195
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 02:20:07 +0200

    Refactor BitStorage class to support multiple files
    This commit refactors the BitStorage class in storage.py to support storing bits in multiple files. It introduces a new directory called "storage" and divides the bits into multiple files based on a given number. The maximum bit value and the maximum bits per file are also defined. The methods for getting the file name, file path, bit index, and byte and offset are added to handle the new file structure. The write_bit and read_bit methods are modified to use the updated file structure.

--- storage.py
+++ storage.py
@@ -3,43 +3,61 @@ import struct
 
 
 class BitStorage:
-    file_path = "cards.bin"
+    directory = "storage"
+    num_files = 100
+    max_bit = 9_999_999_999
+    max_bits_per_file = max_bit+1 // num_files
+
+    def get_file_name(bit_index):
+        file_index = bit_index // BitStorage.max_bits_per_file
+        return f"cards.{file_index}"
+
+    def get_file_path(bit_index):
+        return os.path.join(BitStorage.directory, BitStorage.get_file_name(bit_index))
 
     def create_file_if_not_exists():
         if not os.path.exists(BitStorage.file_path):
-            with open(BitStorage.file_path, 'w'):
+            with open(BitStorage.file_path, "w"):
                 pass
+
+    def get_bit_index(bit_index):
+        return bit_index % BitStorage.max_bits_per_file
+
+    def get_byte_and_offset(bit_index):
+        bit_index = BitStorage.get_bit_index(bit_index)
+        return divmod(bit_index, 8)
+
     @staticmethod
     def write_bit(bit_index, bit_value):
         BitStorage.create_file_if_not_exists()
-        byte_index, offset = divmod(bit_index, 8)
+        byte_index, offset = BitStorage.get_byte_and_offset(bit_index)
 
-        with open(BitStorage.file_path, 'rb+') as file:
+        with open(BitStorage.file_path, "rb+") as file:
             file.seek(byte_index)
 
             try:
-                byte = struct.unpack('B', file.read(1))[0]
+                byte = struct.unpack("B", file.read(1))[0]
             except struct.error:
                 byte = 0
 
             if bit_value == 1:
-                byte |= (1 << offset)
+                byte |= 1 << offset
             else:
                 byte &= ~(1 << offset)
 
             file.seek(byte_index)
-            file.write(struct.pack('B', byte))
+            file.write(struct.pack("B", byte))
 
     @staticmethod
     def read_bit(bit_index):
         BitStorage.create_file_if_not_exists()
-        byte_index, offset = divmod(bit_index, 8)
+        byte_index, offset = BitStorage.get_byte_and_offset(bit_index)
 
-        with open(BitStorage.file_path, 'rb') as file:
+        with open(BitStorage.file_path, "rb") as file:
             file.seek(byte_index)
 
             try:
-                byte = struct.unpack('B', file.read(1))[0]
+                byte = struct.unpack("B", file.read(1))[0]
             except struct.error:
                 byte = 0
 
