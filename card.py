f55520c6643312ad90f0e5a9d30197363986c749
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 04:53:36 +0200

    Refactor card.py for improved readability and maintainability

--- card.py
+++ card.py
@@ -1,9 +1,11 @@
 from storage import BitStorage
 from encryption import DESCipher
 
+
 class Card:
     max_value = 999
     last_card_serial = -1
+
     @staticmethod
     def generate_card_number(serial: int, value: int):
         if value > Card.max_value:
@@ -15,24 +17,28 @@ class Card:
         card_value_str = str(card_value).zfill(3)
 
         card_int = int(card_serial_str + card_value_str)
-        card_bytes = card_int.to_bytes((card_int.bit_length() + 7) // 8, byteorder='big')
+        card_bytes = card_int.to_bytes(
+            (card_int.bit_length() + 7) // 8, byteorder="big"
+        )
 
         card_number_bytes = DESCipher.encrypt(card_bytes)
-        card_number_int = int.from_bytes(card_number_bytes, byteorder='big')
+        card_number_int = int.from_bytes(card_number_bytes, byteorder="big")
 
         return card_number_int
+
     def retrieve_card_info(card_number: int):
-        card_number_bytes = card_number.to_bytes((card_number.bit_length() + 7) // 8, byteorder='big')
+        card_number_bytes = card_number.to_bytes(
+            (card_number.bit_length() + 7) // 8, byteorder="big"
+        )
 
         decrypted_bytes = DESCipher.decrypt(card_number_bytes)
-        card_int = int.from_bytes(decrypted_bytes, byteorder='big')
+        card_int = int.from_bytes(decrypted_bytes, byteorder="big")
 
         card_serial_str = str(card_int)[:-3]
         card_value_str = str(card_int)[-3:]
 
         card_serial = int(card_serial_str)
         card_value = int(card_value_str)
-
 
         return card_serial, card_value
 
@@ -44,15 +50,17 @@ class Card:
             return True
         else:
             return False
+
+    @staticmethod
     def check_value(card_number: int):
         card_serial, card_value = Card.retrieve_card_info(card_number)
         return card_value
+
     @staticmethod
     def redeem_card(card_number: int):
         card_serial, card_value = Card.retrieve_card_info(card_number)
         BitStorage.write_bit(card_serial, 1)
         return card_value
-
 
 
 def test():
@@ -63,6 +71,7 @@ def test():
     print(Card.check_redemption(card_number))
     print(Card.redeem_card(card_number))
     print(Card.check_redemption(card_number))
+
 
 if __name__ == "__main__":
     test()
