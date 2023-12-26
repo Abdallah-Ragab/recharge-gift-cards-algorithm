1bc6e6aac65bbc5d0ecc90b05f48279a1637ba37
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 06:46:58 +0200

    Refactor CLI commands to improve code readability

--- cli2.py
+++ cli2.py
@@ -17,36 +17,33 @@ def buy(value):
 @click.argument("card_number", type=int)
 def value(card_number):
     try:
-        Card.validate_card_number(card_number)
+        card_value = Card.check_value(card_number)
+        click.echo(f"Card value: {card_value}")
     except ValueError as e:
         click.echo(e)
         return
 
-    card_value = Card.check_value(card_number)
-    click.echo(f"Card value: {card_value}")
 
 @cli.command()
 @click.argument("card_number", type=int)
 def redeem(card_number):
     try:
-        Card.validate_card_number(card_number)
+        card_value = Card.redeem_card(card_number)
+        click.echo(f"Card value: {card_value}")
     except ValueError as e:
         click.echo(e)
         return
 
-    card_value = Card.redeem_card(card_number)
-    click.echo(f"Card value: {card_value}")
 
 @cli.command()
 @click.argument("card_number", type=int)
 def check(card_number):
     try:
-        Card.validate_card_number(card_number)
+        card_redeemed = Card.check_redemption(card_number)
     except ValueError as e:
         click.echo(e)
         return
 
-    card_redeemed = Card.check_redemption(card_number)
     if card_redeemed:
         click.echo("Card has been redeemed.")
     else:
