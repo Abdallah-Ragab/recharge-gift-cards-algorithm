7039c828f707cd080365ec5ea5eaeb1f64da28e2
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-26 06:44:17 +0200

    Refactor CLI commands to improve code readability

--- CLI.py
+++ CLI.py
@@ -11,36 +11,32 @@ class CLI(cmd.Cmd):
         """Check the value of a card."""
         card_number = int(arg)
         try:
-            Card.validate_card_number(card_number)
+            card_value = Card.check_value(card_number)
+            print(f"Card value: {card_value}")
         except ValueError as e:
             print(e)
-            return
 
-        card_value = Card.check_value(card_number)
-        print(f"Card value: {card_value}")
 
     def do_redeem(self, arg):
         """Redeem a card."""
         card_number = int(arg)
         try:
-            Card.validate_card_number(card_number)
+            card_value = Card.redeem_card(card_number)
+            print(f"Card value: {card_value}")
         except ValueError as e:
             print(e)
             return
 
-        card_value = Card.redeem_card(card_number)
-        print(f"Card value: {card_value}")
 
     def do_check(self, arg):
         """Check if a card has been redeemed."""
         card_number = int(arg)
         try:
-            Card.validate_card_number(card_number)
+            card_redeemed = Card.check_redemption(card_number)
         except ValueError as e:
             print(e)
             return
 
-        card_redeemed = Card.check_redemption(card_number)
         if card_redeemed:
             print("Card has been redeemed.")
         else:
